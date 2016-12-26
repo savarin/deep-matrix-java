e9287713313a87e1b8d62cde19c03ddaf55e3974
Ezzeri Esa <esa@squareup.com>
2016-12-26 13:41:49 +0700

    changed float to double, added identity matrix

--- Matrix.java
+++ Matrix.java
@@ -1,12 +1,21 @@
+import java.util.Random;
+
+
 public class Matrix {
   private int m;
   private int n;
-  private float[][] entries;
+  private double[][] entries;
 
-  public Matrix(float[][] entries) {
+  public Matrix(int m, int n) {
+    this.m = m;
+    this.n = n;
+    entries = new double[m][n];
+  }
+
+  public Matrix(double[][] entries) {
     m = entries.length;
     n = entries[0].length;
-    this.entries = new float[m][n];
+    this.entries = new double[m][n];
 
     for (int i=0; i<m; i++) {
       for (int j=0; j<n; j++) {
@@ -15,11 +24,48 @@ public class Matrix {
     }
   }
   
+  public static Matrix identity(int n) {
+    Matrix I = new Matrix(n, n);
+
+    for (int i=0; i<n; i++) {
+      for (int j=0; j<n; j++) {
+        I.entries[i][j] = 1.0;
+      }
+    }
+
+    return I;
+  }
+
+  public static Matrix identity(int n, double a) {
+    Matrix I = new Matrix(n, n);
+
+    for (int i=0; i<n; i++) {
+      for (int j=0; j<n; j++) {
+        I.entries[i][j] = a;
+      }
+    }
+
+    return I;
+  }  
+
+  public static Matrix random(int m, int n) {
+    Matrix R = new Matrix(m, n);
+    Random random = new Random();
+
+    for (int i=0; i<m; i++) {
+      for (int j=0; j<n; j++) {
+        R.entries[i][j] = random.nextGaussian();
+      }
+    }
+
+    return R;
+  }
+
   public Matrix dot(Matrix B) {
     Matrix A = this;
-    if (A.n != B.m) throw new RuntimeException("Improper matrix shapes.");
+    if (A.n != B.m) throw new RuntimeException("Improper matrix shape.");
 
-    float [][] c = new float[A.m][B.n];
+    double [][] c = new double[A.m][B.n];
     Matrix C = new Matrix(c);
 
     for (int i=0; i<A.m; i++) {
@@ -43,15 +89,22 @@ public class Matrix {
   }
 
   public static void main(String[] args) {
-    float [][] a = {{1, 2}, {2, 1}};
-    float [][] b = {{1, 0, 3}, {0, 1, 3}, {0, 1, 3}};
+    double [][] a = {{1, 2}, {2, 1}};
+    double [][] b = {{1, 0}, {0, 1}};
 
     Matrix A = new Matrix(a);
     Matrix B = new Matrix(b);
     Matrix C = new Matrix(a);
 
     C = A.dot(B);
-
     C.print();
+
+    Matrix W = new Matrix(2, 2);
+    W = Matrix.random(2, 2);
+    W.print();
+
+    Matrix I = new Matrix(2, 2);
+    W = Matrix.identity(2);
+    W.print();
   }
 }
