e73f67e2f569780e25d3092288c3a1d67b4f6f4f
Ezzeri Esa <esa@squareup.com>
2016-12-26 12:35:12 +0700

    created class and dot

--- Matrix.java
+++ Matrix.java
@@ -1,23 +1,57 @@
 public class Matrix {
+
+  private int m;
+  private int n;
+  private float[][] entries;
+
+  public Matrix(float[][] entries) {
+    m = entries.length;
+    n = entries[0].length;
+    this.entries = new float[m][n];
+
+    for (int i=0; i<m; i++) {
+      for (int j=0; j<n; j++) {
+        this.entries[i][j] = entries[i][j];
+      }
+    }
+  }
   
-  public static void main(String[] args) {
-    float [][] A = {{1, 2}, {2, 1}};
-    float [][] B = {{1, 0}, {0, 1}};
+  public Matrix dot(Matrix B) {
+    Matrix A = this;
 
-    float [][] C = new float[2][2];
+    float [][] c = new float[A.m][B.n];
+    Matrix C = new Matrix(c);
 
-    for (int i=0; i<A.length; i++) {
-      for (int j=0; j<B[0].length; j++) {
-        for (int k=0; k<A[0].length; k++) {
-          C[i][j] += A[i][k] * B[k][j];
+    for (int i=0; i<A.m; i++) {
+      for (int j=0; j<B.n; j++) {
+        for (int k=0; k<A.n; k++) {
+          C.entries[i][j] += A.entries[i][k] * B.entries[k][j];
         }
       }
     }
 
-    for (int i=0; i<C.length; i++) {
-      for (int j=0; j<C[0].length; j++) {
-        System.out.println(C[i][j]);
+    return C;
+  }
+
+  public void print() {
+    for (int i=0; i<m; i++) {
+      for (int j=0; j<n; j++) {
+        System.out.printf("%.1f ", entries[i][j]);
       }
+      System.out.println();
     }
+  }
+
+  public static void main(String[] args) {
+    float [][] a = {{1, 2}, {2, 1}};
+    float [][] b = {{1, 0}, {0, 1}};
+
+    Matrix A = new Matrix(a);
+    Matrix B = new Matrix(b);
+    Matrix C = new Matrix(a);
+
+    C = A.dot(B);
+
+    C.print();
   }
 }
